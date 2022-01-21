086835be93732e8ddfe6f4d2cb6c6bb2b04b2154
d-vasudevan <divya@thoughtbot.com>
2022-01-21 10:57:26 -0700

    updated json object

--- src/recipes.js
+++ src/recipes.js
@@ -1,48 +1,93 @@
-const recipes = [{
-    "name" : "something",
-    "description" : "details about the recipe something",
-    "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
-    "ingredients" : [
-        {
-            "ingredient_name" : "a", 
-            "ingredient_quantity" : "amount"
-        },
-        {
-            "ingredient_name" : "b", 
-            "ingredient_quantity" : "amount"
-        },
-        {
-            "ingredient_name" : "c", 
-            "ingredient_quantity" : "amount"
-        }
-    ],
-    "instructions" : [
-        "do x", 
-        "then do y"
-    ]
-},{
-    "name" : "something 2",
-    "description" : "details about the recipe something 2",
-    "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
-    "ingredients" : [
-        {
-            "ingredient_name" : "a", 
-            "ingredient_quantity" : "amount"
-        },
-        {
-            "ingredient_name" : "b", 
-            "ingredient_quantity" : "amount"
-        },
-        {
-            "ingredient_name" : "c", 
-            "ingredient_quantity" : "amount"
-        }
-    ],
-    "instructions" : [
-        "do x", 
-        "then do y"
-    ]
-}
-];
+const allRecipes = {
+    "Breakfast" :
+        [{
+            "name" : "breakfast recipe 1",
+            "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
+            "ingredients" : [
+                {
+                    "ingredient_name" : "a", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "b", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "c", 
+                    "ingredient_quantity" : "amount"
+                }
+            ],
+            "instructions" : [
+                "do x", 
+                "then do y"
+            ]
+        },{
+            "name" : "breakfast recipe 2",
+            "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
+            "ingredients" : [
+                {
+                    "ingredient_name" : "a", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "b", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "c", 
+                    "ingredient_quantity" : "amount"
+                }
+            ],
+            "instructions" : [
+                "do x", 
+                "then do y"
+            ]
+        }],
+    "Rice Side Dishes" :
+        [{
+            "name" : "Rice Side Dishes recipe 1",
+            "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
+            "ingredients" : [
+                {
+                    "ingredient_name" : "a", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "b", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "c", 
+                    "ingredient_quantity" : "amount"
+                }
+            ],
+            "instructions" : [
+                "do x", 
+                "then do y"
+            ]
+        },{
+            "name" : "Rice Side Dishes recipe 2",
+            "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
+            "ingredients" : [
+                {
+                    "ingredient_name" : "a", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "b", 
+                    "ingredient_quantity" : "amount"
+                },
+                {
+                    "ingredient_name" : "c", 
+                    "ingredient_quantity" : "amount"
+                }
+            ],
+            "instructions" : [
+                "do x", 
+                "then do y"
+            ]
+        }]
 
-export default recipes;
+};
+
+export default allRecipes;
