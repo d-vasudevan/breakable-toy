a7b6788d7b6521e5c2403f224c426719d890674a
d-vasudevan <divya@thoughtbot.com>
2022-01-21 07:17:02 -0700

    displaying a list of recipe names

--- src/App.js
+++ src/App.js
@@ -2,37 +2,40 @@ import './App.css';
 import React from 'react';
 import recipes from './recipes.js';
 
-console.log(recipes);
-
-class Test extends React.Component {
-  render () {
-    return (
-      <div>
-        <p class="text-gray-500">{recipes.name}</p>
-      </div>
-    );
-  }
-}
-
 class Header extends React.Component {
   render () {
     return (
       <div>
         <h1>My Recipes</h1>
-        <div>
-          <p>search feature</p>
-          <p>filter feature</p>
-        </div>
       </div>
     );
   }
 }
 
+function NumberList(props) {
+  const listItems = recipes.map((recipe) =>
+    <li>{recipe.name}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+// class Test extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <p class="text-gray-500">{recipes[0].name}</p>
+//       </div>
+//     );
+//   }
+// }
+
 function App() {
   return (
     <div>
       <Header />
-      <Test />
+      <NumberList />
     </div>
   );
 }
--- src/recipes.js
+++ src/recipes.js
@@ -1,5 +1,7 @@
-const recipes = {
+const recipes = [{
     "name" : "something",
+    "description" : "details about the recipe something",
+    "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
     "ingredients" : [
         {
             "ingredient_name" : "a", 
@@ -16,8 +18,31 @@ const recipes = {
     ],
     "instructions" : [
         "do x", 
-        "the do y"
+        "then do y"
     ]
-};
+},{
+    "name" : "something 2",
+    "description" : "details about the recipe something 2",
+    "img_url" : "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg",
+    "ingredients" : [
+        {
+            "ingredient_name" : "a", 
+            "ingredient_quantity" : "amount"
+        },
+        {
+            "ingredient_name" : "b", 
+            "ingredient_quantity" : "amount"
+        },
+        {
+            "ingredient_name" : "c", 
+            "ingredient_quantity" : "amount"
+        }
+    ],
+    "instructions" : [
+        "do x", 
+        "then do y"
+    ]
+}
+];
 
 export default recipes;
